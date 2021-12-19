be125a24eee12aa93b95144465f26d356ef7357a
Rodrigo Carneiro Andrade <andrade.rodrigo@USCMAC192.local>
2021-12-19 18:16:38 -0300

    feature: create styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
+import * as S from './styles';
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, React Js, Next JS e Styled Components
+    </S.Description>
+
+    <S.Ilustration
+      src="/img/hero-illustration.svg"
+      alt="Imagem de um dev em frente para uma tela com código"
+    />
+  </S.Wrapper>
 );
 
 export default Main;
